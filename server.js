508f4bb342c48d8024d0ef40bb38837e24df8312
Allana Anderson <allana.anderson17@gmail.com>
2020-07-23 14:43:13 -0600

    fixed bugs keeping server from listening

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var http = require("http");
+
 var express = require("express");
 var path = require("path");
 
 var app = express();
 var PORT = 3000;
 
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-//reservations data
+//reservations data, array of objects to be filled with user input
 let reservations = [];
 let watiList = [];
 
